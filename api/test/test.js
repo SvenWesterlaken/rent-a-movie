d322be0bd0e73b77d51a57d0eb8c3f26b19900a1
SVEN-LAPTOP\Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-06-14 12:37:10 +0200

    Hopefully a finished build

---
+++ api/test/test.js
@@ -0,0 +1,18 @@
+var chai = require('chai'),
+    chai_http = require('chai-http'),
+    server = require('../index.js'),
+    should = chai.should();
+
+chai.use(chai_http);
+
+describe('API test', function() {
+  it('Test GET /api/v1/film', function(done) {
+    chai.request(server)
+      .get('/hello')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.equal({"msg": "Hi, have a nice NodeJS day."});
+        done();
+      });
+  });
+});

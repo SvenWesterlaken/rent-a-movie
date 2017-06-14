24e10334e0d14815149ed0f10ccd41567008981b
SVEN-LAPTOP\Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-06-14 13:25:45 +0200

    Error fixes again

--- api/routes/api_v1.js
+++ api/routes/api_v1.js
@@ -2,6 +2,6 @@ var express = require('express'),
     router = express.Router(),
     path = require('path'),
     pool = require('../database/db'),
-    auth =  require('../auth/auth'),
+    auth =  require('../auth/auth');
 
 module.exports = router;

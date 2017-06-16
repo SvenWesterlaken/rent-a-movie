19422c5ed07f3aacbbfcfa98b64e42f5429a455e
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-06-16 15:34:19 +0200

    Added endpoint for adding rental with user and inventory ID's

--- api/routes/api_v1.js
+++ api/routes/api_v1.js
@@ -133,31 +133,30 @@ router.get('/rentals/:userid', function (req, res) {
 
 });
 
-router.post('rentals/:userid/:inventoryid', function (req, res) {
-    var rental_id = req.body.rental_id || '',
-        rental_date = req.body.rental_date || '',
-        inventory_id = req.body.inventory_id ||'',
-        custormer_id = req.body.customer_id || '',
-        return_date = req.body.return_date || '',
-        staff_id = req.body.staff_id || ''
-    // var inventoryId = get('inventory_id');
-    // var customerId = get('customer_id');
-    // var returnDate = get('return_date');
-    // var staffId = get('staff_id');
-    // var lastUpdate = get('last_update')
+router.post('/rentals/:userid/:inventoryid', function (req, res) {
+  var inventoryID = req.params.inventoryid || 0,
+      userID = req.params.userid || 0,
+      staffID = Math.floor((Math.random() * 5) + 1);
 
-    pool.query(
-        'INSERT INTO rental (rental_id, rental_date, inventory_id, customer_id, return_date, staff_id, last_update)' +
-        'VALUES ("' + rental_id + '", "' + return_date + '", "' + inventory_id + '", "' + custormer_id + '", "' + return_date + '", "' + staff_id + '",)',
-        function (error,rows,fields) {
-            if (error)
-                throw error;
-            else {
-                res.status(200);
-                res.contentType('application/json');
-                res.json(rows);
-            }
-        });
+  if(isNaN(userID) || userID == 0) {
+    res.status(401).json({"error" : "No proper user ID given"});
+  } else if (isNaN(inventoryID) || userID == 0) {
+    res.status(401).json({"error" : "No proper inventory ID given"});
+  } else {
+    var query = 'INSERT INTO rental (inventory_id, customer_id, rental_date, return_date, staff_id) VALUES ' +
+                '("' + inventoryID + '", "' + userID + '", CURRENT_TIMESTAMP, DATE_ADD(NOW(), INTERVAL 3 WEEK), "' + staffID + '")';
+
+
+    pool.getConnection( function(error, connection) {
+      if (error) { throw error }
+      connection.query(query, function (error, rows, fields) {
+    	  if (error) {
+          throw error;
+        }
+    	  res.status(200).end(JSON.stringify(rows));
+    	});
+    });
+  }
 });
 
 // Hier wordt de lijst van alle films getoond
